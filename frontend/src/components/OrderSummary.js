2870e27a0a9eaff9f73119777a2f3ec2e64a41a3
Patty <patriciamarie0816@gmail.com>
2024-10-21 19:52:42 +0800

    styled checkout page

--- frontend/src/components/OrderSummary.js
+++ frontend/src/components/OrderSummary.js
@@ -3,15 +3,20 @@
 const OrderSummary = ({ orders, handleSubmit}) => {
 
     return (
-        <div>
-            {orders.drinks.map((item, index) => (
-                <li key={index}>
-                    <span>Drink ID: {item.drink}</span>
-                    <span>Name: {item.name}</span>
-                    <span>Quantity: {item.quantity}</span>
-                </li>
-            ))}
-            <button onClick={handleSubmit}>Finalize drinks</button>
+        <div className="order-container">
+            <h2>Your Order</h2>
+            <div className="order-list">
+                <ul>
+                    {orders.drinks.map((item, index) => (
+                        <li key={index}>
+                            <span><strong>{item.quantity}</strong></span>
+                            <span>{item.name}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <button className="order-button" onClick={handleSubmit}>Finalize and Place Order</button>
         </div>
     )
 }
